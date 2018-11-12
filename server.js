047416a62d4427e8d56737ae0bcd64e89ced6f06
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-12 15:26:02 -0500

    GET note details endpoint

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 
 // Load array of notes
 const express = require('express');
-
 const data = require('./db/notes');
-
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {res.json(data);});
+
+app.get('/api/notes/:id', (req, res) => {
+  //get the value of in dynamically changing :id
+  const { id } = req.params;
+  //use const 
+  const reqNote = data.find(item => item.id === Number(id));
+  res.json(reqNote);
+});
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
