ea60f90cb29c9a7d083dcc7e6164eae55a98c431
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-12 14:39:55 -0500

    added static files

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 'use strict';
 
 // Load array of notes
+const express = require('express');
+
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express();
 
-// INSERT EXPRESS APP CODE HERE...
+app.use(express.static("public"));
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
